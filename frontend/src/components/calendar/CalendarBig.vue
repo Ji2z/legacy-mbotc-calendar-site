0ec92a4321420b8fe5a557cf54cf73208ebaca5b
wizard9582 <qkfskan82@naver.com>
2021-10-28 17:48:24 +0900

    [S05P31A103-90] fix : 새로고침시 사이드바 초기화이슈 수정

--- frontend/src/components/calendar/CalendarBig.vue
+++ frontend/src/components/calendar/CalendarBig.vue
@@ -63,31 +63,12 @@
             </table>
         </div>
         <div>
-            <div class="fixed right-16 bottom-16">
-                <svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 512.000000 512.000000" preserveAspectRatio="xMidYMid meet"
-                class="fill-current text-blue-700 cursor-pointer hover:text-blue-800" @click="goNotice">
-                    <g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)" stroke="none">
-                    <path d="M2370 5113 c-379 -36 -661 -116 -980 -278 -378 -193 -717 -497 -965
-                    -865 -104 -156 -232 -419 -294 -605 -49 -150 -89 -321 -113 -490 -17 -118 -17
-                    -512 0 -630 42 -295 120 -553 242 -800 137 -280 272 -468 494 -691 221 -220
-                    412 -357 681 -489 188 -92 309 -137 500 -185 500 -126 1002 -102 1490 71 149
-                    53 407 182 540 271 299 199 573 480 769 788 72 113 188 353 235 486 235 662
-                    194 1372 -115 1993 -124 250 -263 447 -458 648 -216 224 -428 378 -711 518
-                    -296 146 -572 225 -900 255 -102 9 -333 11 -415 3z m304 -1253 c20 -14 49 -43
-                    64 -64 l27 -39 5 -491 5 -491 491 -5 491 -5 39 -27 c21 -15 50 -44 64 -65 21
-                    -31 25 -48 25 -113 0 -65 -4 -82 -25 -113 -14 -21 -43 -50 -64 -65 l-39 -27
-                    -491 -5 -491 -5 -5 -491 -5 -491 -27 -39 c-15 -21 -44 -50 -65 -64 -31 -21
-                    -48 -25 -113 -25 -65 0 -82 4 -113 25 -21 14 -50 43 -65 64 l-27 39 -5 491 -5
-                    491 -491 5 -491 5 -39 27 c-62 44 -88 90 -92 167 -4 82 23 141 87 186 l43 30
-                    491 5 492 5 5 492 5 491 30 43 c45 64 104 91 186 87 50 -3 75 -10 103 -28z"/>
-                    </g>
-                </svg>
-            </div>
+            <main-addon class="fixed right-16 bottom-16" @click="goEdit"/>
         </div>
     </div>
 </template>
 <script>
-// import abc from '@/components/'
+import MainAddon from '@/components/main/MainAddon.vue'
 import { reactive } from 'vue'
 // import { useStore } from 'vuex'
 import { useRouter } from 'vue-router'
@@ -95,6 +76,7 @@ import { useRouter } from 'vue-router'
 export default {
     name: 'CalendarBig',
     components: {
+        MainAddon
     },
     setup(props, {emit}){
         const router = useRouter()
@@ -164,8 +146,11 @@ export default {
         const goNotice = ()=>{
             router.push("notice")
         }
+        const goEdit = ()=>{
+            router.push("/main/notice")
+        }
         init()
-        return { state, monthList, nextMonth, beforeMonth, goDetail, goNotice }
+        return { state, monthList, nextMonth, beforeMonth, goDetail, goNotice, goEdit }
     }
 };
 </script>
