dc534de689950275d68ff3ccbe78b3996b317c0e
신지예 <guriguri8833@gmail.com>
2021-10-29 16:06:07 +0900

    feat : command - register

--- plugin/server/command.go
+++ plugin/server/command.go
@@ -1,20 +1,51 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"strings"
 
 	"github.com/mattermost/mattermost-plugin-api/experimental/command"
 	"github.com/mattermost/mattermost-server/v5/model"
-	"github.com/mattermost/mattermost-server/v6/plugin"
+	"github.com/mattermost/mattermost-server/v5/plugin"
 	"github.com/pkg/errors"
 )
 
-const helpText = "###### Mattermost MBotC Plugin - Slash Command Help\n" +
-	"* `/mbotc help` - 도움말\n" +
-	"* `/mbotc register` - 글을 등록할 수 있습니다."
+func (p *Plugin) getCommand() (*model.Command, error) {
+	iconData, err := command.GetIconData(p.API, "assets/mbotc-icon.svg")
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get icon data")
+	}
 
-const defaultRoomText = "not set (using your Mattermost email as the default)"
+	return &model.Command{
+		Trigger:              "mbotc",
+		DisplayName:          "mbotc",
+		Description:          "Integration with MBotC.",
+		AutoComplete:         true,
+		AutoCompleteDesc:     "Available commands: help, term, date",
+		AutoCompleteHint:     "[command]",
+		AutocompleteData:     getAutocompleteData(),
+		AutocompleteIconData: iconData,
+	}, nil
+}
+
+const helpText = "###### Mattermost MBotC Plugin - Slash Command Help\n" +
+	"* `/mbotc help` - help text\n" +
+	"* `/mbotc term` - Register your Term Notice.\n" +
+	"	```\n" +
+	"	[Template]\n" +
+	"	<write here what you want to notice with markdown format>\n" +
+	"	`date YYYY-MM-DD hh:mm - YYYY-MM-DD hh:mm\n" +
+	"	```\n" +
+	"* `/mbotc date` - Register your Date Notice.\n" +
+	"	```\n" +
+	"	[Template]\n" +
+	"	<write here what you want to notice with markdown format>\n" +
+	"	`date YYYY-MM-DD hh:mm\n" +
+	"	```\n"
 
 type CommandHandlerFunc func(p *Plugin, c *plugin.Context, header *model.CommandArgs, args ...string) *model.CommandResponse
 
@@ -23,17 +54,17 @@ type CommandHandler struct {
 	defaultHandler CommandHandlerFunc
 }
 
-var webexCommandHandler = CommandHandler{
+//===================================================
+// command Handler
+// command : func
+//===================================================
+var mbotcCommandHandler = CommandHandler{
 	handlers: map[string]CommandHandlerFunc{
-		"help":       executeHelp,
-		"info":       executeInfo,
-		"start":      executeStart,
-		"room":       executeRoom,
-		"room-reset": executeRoomReset,
-		"reset-room": executeRoomReset,
-		"join":       executeStartWithArg, // Used as an alias for /webex <@username>/<room id> to allow for Autocomplete suggestions
+		"help": executeHelp,
+		"term": executeTerm,
+		"date": executeDate,
 	},
-	defaultHandler: executeStartWithArg,
+	defaultHandler: executeHelp,
 }
 
 func (ch CommandHandler) Handle(p *Plugin, c *plugin.Context, header *model.CommandArgs, args ...string) *model.CommandResponse {
@@ -55,51 +86,62 @@ func (p *Plugin) help(header *model.CommandArgs) *model.CommandResponse {
 	return &model.CommandResponse{}
 }
 
+func executeTerm(p *Plugin, c *plugin.Context, header *model.CommandArgs, args ...string) *model.CommandResponse {
+	p.postCommandResponse(header, "###### Successfully registered your term notice:\n"+header.Command)
+	p.registerNotice(header)
+	return &model.CommandResponse{}
+}
+
+func executeDate(p *Plugin, c *plugin.Context, header *model.CommandArgs, args ...string) *model.CommandResponse {
+	p.postCommandResponse(header, "###### Successfully registered your term notice:\n"+header.Command)
+	p.registerNotice(header)
+	return &model.CommandResponse{}
+}
+
+// Send Post Request to our Bot server
+func (p *Plugin) registerNotice(commandArgs *model.CommandArgs) {
+	pbytes, _ := json.Marshal(commandArgs)
+	buff := bytes.NewBuffer(pbytes)
+	// http.Post(url, request body MIME type, data)
+	resp, err := http.Post("http://httpbin.org/post", "application/json", buff)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+
+	// Check Response
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err == nil {
+		str := string(respBody)
+		fmt.Println(str)
+	}
+}
+
 func (p *Plugin) ExecuteCommand(c *plugin.Context, commandArgs *model.CommandArgs) (*model.CommandResponse, *model.AppError) {
 	args := strings.Fields(commandArgs.Command)
-	if len(args) == 0 || args[0] != "/webex" {
+	if len(args) == 0 || args[0] != "/mbotc" {
 		return p.help(commandArgs), nil
 	}
 
-	if !p.getConfiguration().IsValid() {
-		return p.responsef(commandArgs, "The Webex plugin has not been configured correctly: the sitename has not been set. Please contact your system administrator."), nil
-	}
-
-	return webexCommandHandler.Handle(p, c, commandArgs, args[1:]...), nil
-}
-
-func (p *Plugin) getCommand() (*model.Command, error) {
-	iconData, err := command.GetIconData(p.API, "assets/icon.svg")
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get icon data")
-	}
-
-	return &model.Command{
-		Trigger:              "mbotc",
-		DisplayName:          "mbotc",
-		Description:          "Integration with MBotC.",
-		AutoComplete:         true,
-		AutoCompleteDesc:     "Available commands: help, register",
-		AutoCompleteHint:     "[command]",
-		AutocompleteData:     getAutocompleteData(),
-		AutocompleteIconData: iconData,
-	}, nil
+	return mbotcCommandHandler.Handle(p, c, commandArgs, args[1:]...), nil
 }
 
 func getAutocompleteData() *model.AutocompleteData {
-	// 여기가 실제로 /mbotc 입력중에 보이는 부분
-	webexAutocomplete := model.NewAutocompleteData("mbotc", "[command]", "Available commands: help, info, start, <room id/@username>, room, room-reset")
+	mbotcAutocomplete := model.NewAutocompleteData("mbotc", "[command]", "Available commands: help, term, date")
 
-	// 여기가 "/mbotc "까지 쳤을 때 보이는 커맨드들
-	help := model.NewAutocompleteData("help", "", "도움말")
-	webexAutocomplete.AddCommand(help)
+	help := model.NewAutocompleteData("help", "", "Guide for mbotc")
+	mbotcAutocomplete.AddCommand(help)
 
-	register := model.NewAutocompleteData("register", "", "글을 등록합니다.")
-	webexAutocomplete.AddCommand(register)
+	term := model.NewAutocompleteData("term", "[text]", "Register your Term Notice (Please refer to /help)")
+	mbotcAutocomplete.AddCommand(term)
 
-	return webexAutocomplete
+	date := model.NewAutocompleteData("date", "[text]", "Register your Date Notice (Please refer to /help)")
+	mbotcAutocomplete.AddCommand(date)
+
+	return mbotcAutocomplete
 }
 
+// Post Message to Channel with Bot
 func (p *Plugin) postCommandResponse(args *model.CommandArgs, text string) {
 	post := &model.Post{
 		UserId:    p.botUserID,
@@ -107,9 +149,4 @@ func (p *Plugin) postCommandResponse(args *model.CommandArgs, text string) {
 		Message:   text,
 	}
 	_ = p.API.SendEphemeralPost(args.UserId, post)
-}
-
-func (p *Plugin) responsef(commandArgs *model.CommandArgs, format string, args ...interface{}) *model.CommandResponse {
-	p.postCommandResponse(commandArgs, fmt.Sprintf(format, args...))
-	return &model.CommandResponse{}
 }
--- plugin/server/plugin.go
+++ plugin/server/plugin.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"io/ioutil"
 	"path/filepath"
-	"strings"
+	"net/http"
 	"sync"
 
-	"github.com/mattermost/mattermost-server/v6/plugin"
+	"github.com/mattermost/mattermost-server/v5/model"
+	"github.com/mattermost/mattermost-server/v5/plugin"
+
+	"github.com/pkg/errors"
 )
 
 const (
@@ -30,74 +32,62 @@ type Plugin struct {
 	// configuration is the active plugin configuration. Consult getConfiguration and
 	// setConfiguration for usage.
 	configuration *configuration
+
+	// KV store
+	store Store
 }
 
+// OnActivate is invoked when the plugin is activated. If an error is returned, the plugin
+// will be terminated. The plugin will not receive hooks until after OnActivate returns
+// without error. OnConfigurationChange will be called once before OnActivate.
 // OnActivate checks if the configurations is valid and ensures the bot account exists
 func (p *Plugin) OnActivate() error {
-	// 웹엑스 설정 가져오기 -> MBotC와 상관 無
-	config := p.getConfiguration()
-
-	// 봇을 생성하고 err 발생 시 err에 값 저장
+	// create a bot
 	botUserID, err := p.Helpers.EnsureBot(&model.Bot{
 		Username:    botUserName,
 		DisplayName: botDisplayName,
 		Description: botDescription,
 	})
-	// err에 값이 들어있으면 return해버리고
+
+	fmt.Printf("%s", botUserID);
+
 	if err != nil {
-		return errors.Wrap(err, "failed to ensure bot account")
+		return errors.Wrap(err, "failed to create bot account")
 	}
-	// botUserID에 성공적으로 값이 들어오면 할당한다.
+	// allocate botUserID if success to create bot account
 	p.botUserID = botUserID
 
-	// 플러그인을 설치했을 경우 그 경로
+	// GetBundlePath returns the absolute path where the plugin's bundle was unpacked.
 	bundlePath, err := p.API.GetBundlePath()
 	if err != nil {
 		return errors.Wrap(err, "couldn't get bundle path")
 	}
 
-	// 번들경로/assets/profile.png에서 파일 읽어옴
-	profileImage, err := ioutil.ReadFile(filepath.Join(bundlePath, "assets", "mbotc-logo.png"))
+	// get bundlePath/assets/profile.png
+	profileImage, err := ioutil.ReadFile(filepath.Join(bundlePath, "assets", "profile.png"))
 	if err != nil {
 		return errors.Wrap(err, "couldn't read profile image")
 	}
 
-	// API 사용해서 bot 프로필 이미지 적용
+	// Set bot profile image
 	if appErr := p.API.SetProfileImage(botUserID, profileImage); appErr != nil {
 		return errors.Wrap(appErr, "couldn't set profile image")
 	}
 
-	// p.store = NewStore(p)
+	p.store = NewStore(p)
 
-	// command.go의 getCommand() - /webex 했을 때 나오는 설명
+	// getCommand() of command.go
 	command, err := p.getCommand()
 	if err != nil {
 		return errors.Wrap(err, "failed to get command")
 	}
 
-	// 86에서 가져온 command 등록
 	err = p.API.RegisterCommand(command)
 	if err != nil {
 		return errors.WithMessage(err, "OnActivate: failed to register command")
 	}
 
 	return nil
-}
-
-func (p *Plugin) GetPluginURLPath() string {
-	return "/plugins/" + manifest.ID
-}
-
-func (p *Plugin) GetPluginURL() string {
-	return strings.TrimRight(p.GetSiteURL(), "/") + p.GetPluginURLPath()
-}
-
-func (p *Plugin) GetSiteURL() string {
-	return *p.API.GetConfig().ServiceSettings.SiteURL
-}
-
-func (p *Plugin) errorf(f string, args ...interface{}) {
-	p.API.LogError(fmt.Sprintf(f, args...))
 }
 
 // ServeHTTP demonstrates a plugin that handles HTTP requests by greeting the world.
