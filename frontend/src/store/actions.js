8d7f77fb3b23ca2c93521238763561611a1dea8d
wizard9582 <qkfskan82@naver.com>
2021-11-01 14:47:52 +0900

    [S05P31A103-98] feat : FE api 연결 준비 완료

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -5,31 +5,105 @@ const serverData = {
     SERVER_URL : ""
 }
 
-export function login({state}, payload){
-    const url = '/api/user/login'
+// user API
+export function userLoginMM({state}, payload){
+    const url = serverData.SERVER_URL + '/api/v4/users/login'
     const body = payload
+    //수정해야됨
 
     return $axios.post(url, body);
 }
 
-// export function updateUser({state}, payload){
-//     const url = '/api/user/'
-//     return $axios({
-//         method: 'patch',
-//         url: url,
-//         headers:{
-//             'jwt':  payload.jwt
-//         },
-//         data:{
-//             'image': payload.image,
-//             'nickname': payload.nickname,
-//         }
-//     })
-// }
+export function userLogin({state}, payload){
+    const url = '/user'
+    const body = {
+        "token" : payload.token,
+        "userEmail" : payload.email,
+        "userName" : payload.userName,
+        "url" : serverData.SERVER_URL,
+        "userId" : payload.userId
+    }
+    return $axios.post(url, body);
+}
 
+export function userTokenRefresh({state}, payload){
+    const url = '/user'
 
-// user API
+    return $axios({
+        method: 'patch',
+        url: url,
+        data:{
+            "userEmail" : payload.email, 
+            "token" : payload.token, 
+            "url" : serverData.SERVER_URL, 
+            "userId" : payload.userId
+        }
+    })
+}
+export function deleteUser({state}, payload){
+    const url = '/user'
+
+    return $axios({
+        method: 'delete',
+        url: url,
+        headers:{
+            'auth':  payload.token
+        },
+        data:{
+            "userEmail" : payload.email, 
+        }
+    })
+}
+export function userSync({state}, payload){
+    const url = '/sync'
+    const headers = { "auth": payload.token }
+    //수정해야됨
+
+    return $axios.get(url, {headers});
+}
+
+export function setUserSetting({state}, payload){
+    const url = '/redis/user'
+    const headers = {
+        'auth' : payload.token
+    }
+    const body = {
+        "teams" : payload.teams,
+        "theme" : payload.theme
+    }
+    return $axios.post(url, {headers}, body);
+}
+
+export function getUserSetting({state}, payload){
+    const url = '/redis/user'
+    const headers = {
+        'auth' : payload.token
+    }
+
+    return $axios.get(url, {headers});
+}
 
 // notice API
+
+export function getMonthNotice({state}, payload){
+    const url = '/notification/month?year=' + payload.year + '&month=' + payload.month
+    const headers = { "auth": payload.token }
+
+    return $axios.get(url, {headers});
+}
+
+export function getDayNotice({state}, payload){
+    const url = '/notification/month?year=' + payload.year + '&month=' + payload.month + '&day=' + payload.day
+    const headers = { "auth": payload.token }
+    
+    return $axios.get(url, {headers});
+}
+
+export function getNoticeDetail({state}, payload){
+    const url = '/notification/post/' + payload.postId
+    const headers = { "auth": payload.token }
+    
+    return $axios.get(url, {headers});
+}
 
 // bot API
